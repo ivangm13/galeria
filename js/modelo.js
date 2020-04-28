952ffc7cb6a387991365aebee07bd4f3062b41d4
ivangm13 <ivangarcia346@gmail.com>
2020-04-28 09:43:45 +0200

    Inicio proyecto. Creados todos los archivos

---
+++ js/modelo.js
@@ -0,0 +1,13 @@
+var url = "https://jsonplaceholder.typicode.com/photos";
+
+function getApi(pUrl){
+    let peticion = new XMLHttpRequest;
+    peticion.open = ('GET',url,true);
+    peticion.send();
+
+    peticion.addEventListener('load',)
+
+}
+
+
+
